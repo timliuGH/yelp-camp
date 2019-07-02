11972928bf02dab5c465c310c458407832c564ed
timliuGH <timsgithub@gmail.com>
2019-07-02 04:21:31 +0000

    Add first RESTful routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const ip = '0.0.0.0';
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 // Array of object campgrounds, each with name and image
@@ -15,8 +18,26 @@ app.get('/', (req, res) => {
     res.render('landing');
 });
 
+// Show campgrounds
 app.get('/campgrounds', (req, res) => {
     res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+// Make new campground and redirect to /campgrounds
+app.post('/campgrounds', (req, res) => {
+    // Get data from form and add to campgrounds array
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    
+    // Redirect back to campgrounds page
+    res.redirect('./campgrounds');
+});
+
+// Show form for adding new campground with post request to /campgrounds
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new');
 });
 
 app.listen(port, ip, () => {
